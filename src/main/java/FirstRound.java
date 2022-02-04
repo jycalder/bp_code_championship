683195369f0ccc4900aea7839cd90e860ba9518f
JHONNY YAMIT CALDERON AMAT <jycalder@pichincha.com>
2022-02-04 10:25:25 -0500

    first round jhonny calderon

--- src/main/java/FirstRound.java
+++ src/main/java/FirstRound.java
@@ -1,19 +1,26 @@
+import java.util.Arrays;
+
 public class FirstRound {
 
   public static void main(String[] args) {
-
+    int[] values = {2 ,1 ,5 ,6 ,10};
+    System.out.println(reports(values));
   }
 
   public static long reports(int[] reports){
-    int[] values = new int[50000000];
+    int[] values = new int[10];
+    Arrays.fill(values,0);
 
-    for(int i = 1; i< reports[0];i++) {
-      values[(i*2)-1]=1;
-      values[i*2]=2;
+    for(int i = 0; i< reports[0];i++) {
+      int start = i == 0 ? 0 : (i*2)-1;
+      int end = i == 0 ? 1 : i*2;
+      values[start]=1;
+      values[end]=2;
     }
     int totalOpen = 0;
     int counter = 0;
     for(int i = 0; i< values.length;i++){
+      System.out.print(values[i] +",");
       if(values[i] == 1){
         ++totalOpen;
       }
@@ -28,6 +35,7 @@ public class FirstRound {
       }
     }
 
+    System.out.println("");
     return counter;
   }
 }
