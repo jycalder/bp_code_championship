249525a48ecaa3b61cad4ad4cc9bcf8eed7a7cb5
JHONNY YAMIT CALDERON AMAT <jycalder@pichincha.com>
2022-02-04 10:04:53 -0500

    first round jhonny calderon

---
+++ src/main/java/FirstRound.java
@@ -0,0 +1,33 @@
+public class FirstRound {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static long reports(int[] reports){
+    int[] values = new int[50000000];
+
+    for(int i = 1; i< reports[0];)
+    {
+      values[i-1]=1;
+      values[i]=2;
+      i+=2;
+    }
+    int totalOpen = 0;
+    int counter = 0;
+    for(int i = 0; i< values.length;i++){
+      if(values[i] == 1){
+        ++totalOpen;
+      }
+      else if( values[1] == 2)
+      {
+        --totalOpen;
+      }
+      if(totalOpen> 0){
+        ++counter;
+      }
+    }
+
+    return counter;
+  }
+}
